2b9de39f3237f63fe19e91e658997b80d06740cd
JaidityaSinha <jaidityasinha200@gmail.com>
2026-07-03 15:36:38 +0530

    channel attention unet trained on RESIDE-6K, 60 epochs: PSNR 28.23, SSIM 0.9563

--- eval.py
+++ eval.py
@@ -6,8 +6,8 @@ from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 import numpy as np
 
-from config import IMAGE_SIZE, MODEL_PATH, MODEL_PATH_6K,TEST_HAZY_DIR, TEST_CLEAR_DIR, MODEL_PATH_CHANNEL
-from models.unet import UNet
+from config import IMAGE_SIZE, MODEL_PATH, MODEL_PATH_6K,TEST_HAZY_DIR, TEST_CLEAR_DIR, MODEL_PATH_CHANNEL, MODEL_PATH_CHANNEL_6K
+from models.unet_channel_attention import ChannelAttentionUNet
 
 
 def evaluate():
@@ -15,9 +15,9 @@ def evaluate():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    model = UNet().to(device)
+    model = ChannelAttentionUNet().to(device)
     model.load_state_dict(
-        torch.load(MODEL_PATH_6K, map_location=device, weights_only=True)
+        torch.load(MODEL_PATH_CHANNEL_6K, map_location=device, weights_only=True)
     )
     model.eval()
 
